0bbabc41e2af889bb0f2026316a8649320e0b4f4
Leovem <misaelvediaeyz@gmail.com>
2024-10-18 14:32:15 -0400

    Primer commit: descripción de tu proyecto

---
+++ backend/src/main/java/com/backend/Security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.backend.Security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())  // Deshabilita CSRF
+            .authorizeHttpRequests(authz -> authz
+                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/auth/users").permitAll()  // Permite acceso público a rutas específicas
+                .anyRequest().authenticated()  // Requiere autenticación para otras rutas
+            );
+    
+        return http.build();
+    }
+
+    
+
+    // Bean para encriptar las contraseñas (opcional, pero recomendado)
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ frondendmovil/android/app/src/main/kotlin/com/example/frondendmovil/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frondendmovil
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
